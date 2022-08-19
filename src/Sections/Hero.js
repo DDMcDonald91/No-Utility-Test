90c70846d997d9b9f325307f2e41b3d4879c487a
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2022-08-19 18:17:10 -0500

    small update

--- src/Sections/Hero.js
+++ src/Sections/Hero.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 export default function Hero() {
   return (
       <Container fluid id="hero-container" className='d-flex align-items-center justify-content-center'>
-        <Container id='hero-title-container' className='w-auto'>
+        <Container id='hero-title-container'>
           <h1 id="hero-title">No Utility Belts</h1>
         </Container>
       </Container>
